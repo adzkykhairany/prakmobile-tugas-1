ed27cd3ab66120a77da497d5f9121017f7330ee8
adzkykhairany <adzkyykhairany@gmail.com>
2023-09-13 21:43:35 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print(
+        "Perkenalkan, nama saya $nama dengan nim $nim dan usia saya ${DateTime.now().year - tahunLahir} tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021091", "Athiyya Adzky Khairany", 2003);
+  mhs.perkenalan();
 }
